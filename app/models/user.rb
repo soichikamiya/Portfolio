07466ff3a9e3371ddf5b51fef94a9dfec3115568
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2018-07-26 07:54:41 +0000

    like & bcrypt 機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :posts, dependent: :destroy
+  
   validates :name, {presence: true, uniqueness: true, length: { in: 3..30 } }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
-  validates :password, {presence: true, length: {minimum: 3}}
+  validates :password, {length: {minimum: 3}}
   
   def posts
-    return Post.where(user_id: self.id)
+    return Post.where(user_id: self.id).order(created_at: :desc)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725121735) do
+ActiveRecord::Schema.define(version: 20180726065816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -26,10 +33,10 @@ ActiveRecord::Schema.define(version: 20180725121735) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "image_name"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
